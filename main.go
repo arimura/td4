461320ec6d5841d1655c940b02530e3e4f44173c
arimura <33759663+arimura@users.noreply.github.com>
2024-09-20 09:41:49 +0900

    Impl add instruction

--- main.go
+++ main.go
@@ -7,6 +7,32 @@ import (
 	"os"
 )
 
+type register int
+
+const (
+	a register = iota
+	b
+)
+
+//Instruction format
+//    MSB <----------------------> LSB
+//bit| 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+//   |Op Cod e       |Immediate data |
+
+type add struct {
+	// Register name
+	register register
+	// Immediate binary data, e.g., "0010"
+	im string
+}
+
+func (x *add) bin() {
+	//   MSB <-> LSB
+	//ADD A: 0000
+	//ADD B: 0101
+
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		log.Fatal("No file name")
