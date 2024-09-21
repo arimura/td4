c60798d0a0c65b8a080765677f83bbbaf9074bb7
arimura <33759663+arimura@users.noreply.github.com>
2024-09-21 11:05:41 +0900

    Impl assembler

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 type register int
@@ -19,6 +21,10 @@ const (
 //bit| 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
 //   |Op Code        |Immediate data |
 
+type instruction interface {
+	gen()
+}
+
 type add struct {
 	// Register name
 	register register
@@ -26,19 +32,78 @@ type add struct {
 	im string
 }
 
-func (x *add) bin() {
+func (x *add) gen() {
 	//   MSB <-> LSB
 	//ADD A: 0000
 	//ADD B: 0101
-
-}
-
-func main() {
-	if len(os.Args) < 2 {
-		log.Fatal("No file name")
+	var s int8
+	if x.register == a {
+		s = 0b0000
+	} else {
+		s = 0b0101
+	}
+	for _, c := range x.im {
+		s = (s << 1) | int8(c-'0')
 	}
 
-	f, err := os.Open(os.Args[1])
+	if *binaryOption {
+		fmt.Printf("%b\n", s)
+	} else {
+		fmt.Printf("%X\n", s)
+	}
+}
+
+func consume(str *string, keyword string) bool {
+	if strings.HasPrefix(*str, keyword) {
+		*str = strings.TrimPrefix(*str, keyword)
+		return true
+	}
+	return false
+}
+
+func consumeBinary(str *string) string {
+	var b []rune
+	for i, c := range *str {
+		if c == '1' || c == '0' {
+			b = append(b, c)
+		} else {
+			*str = (*str)[i:]
+		}
+	}
+	return string(b)
+}
+
+func gen(l string) {
+	var i instruction
+	if consume(&l, "ADD ") {
+		var addi add
+		if consume(&l, "A, ") {
+			addi = add{register: a}
+		} else if consume(&l, "B, ") {
+			addi = add{register: b}
+		} else {
+			log.Fatal("Unsupported Operand")
+		}
+		im := consumeBinary(&l)
+		if im == "" {
+			log.Fatal("Unsupported Immediate data")
+		}
+		addi.im = im
+		i = &addi
+	} else {
+		log.Fatal("Unsupported Op Code")
+	}
+	i.gen()
+}
+
+var binaryOption *bool
+
+func main() {
+	binaryOption = flag.Bool("b", false, "Specify output as binary format")
+	ff := flag.String("f", "", "Specify assembly")
+	flag.Parse()
+
+	f, err := os.Open(*ff)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -47,7 +112,7 @@ func main() {
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
+		gen(line)
 	}
 
 	if err := scanner.Err(); err != nil {
